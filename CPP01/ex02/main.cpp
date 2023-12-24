45e31eda999d5c4ebf1704efb806701585803bfb
LilHenri75 <LilHenri75@github.com>
2023-12-24 12:16:31 +0100

    CPP01/ex02 done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hebernar <hebernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 15:55:54 by hebernar          #+#    #+#             */
+/*   Updated: 2023/12/24 12:12:58 by hebernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main ()
+/*
+Write a program that contains:
+• A string variable initialized to "HI THIS IS BRAIN".
+• stringPTR: A pointer to the string.
+• stringREF: A reference to the string.
+Your program has to print:
+• The memory address of the string variable.
+• The memory address held by stringPTR.
+• The memory address held by stringREF.
+And then:
+• The value of the string variable.
+• The value pointed to by stringPTR.
+• The value pointed to by stringREF.
+*/
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Memory address of the string variable: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string variable: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	return 0;
+}
